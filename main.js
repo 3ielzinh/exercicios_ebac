b2e3649a0d132ae9657884c03769f7a3a41b22c2
3ielzinh <gsjorgee@gmail.com>
2023-07-18 11:44:25 -0300

    Ajustanto o código

--- main.js
+++ main.js
@@ -4,15 +4,14 @@ form.addEventListener('submit', verificaNumero);
 function verificaNumero(event) {
     event.preventDefault();
 
-    const primeiroNumero = document.getElementById('number-a').valueAsNumber;
-    const segundoNumero = document.getElementById('number-b').valueAsNumber;
-    const value = document.getElementById('value');
+    const primeiroNumero = document.getElementById('number-a');
+    const segundoNumero = document.getElementById('number-b');
     let description = '';
     document.getElementById('infos').classList.remove('hidden');
 
-    if (segundoNumero > primeiroNumero){
+    if (segundoNumero.valueAsNumber > primeiroNumero.valueAsNumber){
         description = 'O valor de B é maior do que o valor de A.' 
-    } else if (segundoNumero < primeiroNumero){
+    } else if (segundoNumero.valueAsNumber < primeiroNumero.valueAsNumber){
         description = 'O valor de B é menor do que o valor de A.'
     } else {
         description = 'O valor de B é igual ao valor de A.'
